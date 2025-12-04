057b1ab42c082547f4cc6a25ec5b03c2a5a9943f
Gu, Park <doctorgu@kakao.com>
2025-12-04 12:27:41 +0900

    test

--- test/console/console.py
+++ test/console/console.py
@@ -1,14 +1,24 @@
 """db_client_test"""
 
+import os
+import sys
+from dotenv import load_dotenv
+
+# psycopg2_client
+sys.path.append(__file__[0 : __file__.find("psycopg2_client") + len("psycopg2_client")])
+
+# pylint: disable=wrong-import-position
 from psycopg2_client import Psycopg2Client
 from psycopg2_client_settings import Psycopg2ClientSettings
 
+load_dotenv()
+
 db_settings = Psycopg2ClientSettings(
-    password="0000",
-    host="127.0.0.1",
-    port=5432,
-    database="postgres",
-    user="postgres",
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    database=os.getenv("DB_DATABASE"),
+    user=os.getenv("DB_USER"),
     minconn=3,
     maxconn=6,
     connect_timeout=3,
---
+++ test/flask/app.py
@@ -0,0 +1,47 @@
+"""flask app"""
+
+import os
+import sys
+from flask import Flask
+from dotenv import load_dotenv
+
+# psycopg2_client
+sys.path.append(__file__[0 : __file__.find("psycopg2_client") + len("psycopg2_client")])
+
+# pylint: disable=wrong-import-position
+from psycopg2_client import Psycopg2Client
+from psycopg2_client_settings import Psycopg2ClientSettings
+
+app = Flask(__name__)
+
+load_dotenv()
+
+db_settings = Psycopg2ClientSettings(
+    password=os.getenv("DB_PASSWORD"),
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    database=os.getenv("DB_DATABASE"),
+    user=os.getenv("DB_USER"),
+    minconn=3,
+    maxconn=6,
+    connect_timeout=3,
+    use_en_ko_column_alias=True,
+    use_conditional=True,
+)
+
+
+@app.route("/")
+def stat():
+    """stat"""
+
+    with Psycopg2Client(db_settings=db_settings) as db_client:
+        search = "stat"
+        rows_table = db_client.read_rows(
+            "read_schema", {"is_table": True, "search_percent": f"%{search}%"}
+        )
+        rows_column = db_client.read_rows(
+            "read_schema", {"is_table": False, "search_percent": f"%{search}%"}
+        )
+        return (
+            f"len(rows_table): {len(rows_table)}, len(rows_column): {len(rows_column)}"
+        )
